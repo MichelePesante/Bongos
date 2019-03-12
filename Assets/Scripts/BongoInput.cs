bca9be6632b47fee3c7259cdb4704c77465d7752
giacomo_pignoni <giacomo.pignoni@gmail.com>
2019-03-12 19:30:24 +0100

    Aggiunto script per input

---
+++ Assets/Scripts/BongoInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BongoInput : MonoBehaviour
+{
+    private Collider2D collider2D;
+
+    void Awake()
+    {
+        collider2D = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                if(Input.GetTouch(i).phase == TouchPhase.Began)
+                {
+                    Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+                    if (collider2D.OverlapPoint(touchPos))
+                    {
+                        print("Touch");
+                    }
+                }
+            }
+        }
+    }
+}
